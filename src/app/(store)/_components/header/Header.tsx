34ff3ea98cc9e6fb32540d29a591a69700bb60a4
codebudy5247 <a.shekhar5247@gmail.com>
2024-04-22 12:11:01 +0530

    update navbar

--- src/app/(store)/_components/header/Header.tsx
+++ src/app/(store)/_components/header/Header.tsx
@@ -2,13 +2,10 @@
 
 import Link from "next/link";
 import { Collections } from "@/types";
-import { LucideIcon } from "lucide-react";
 import Logo from "@/components/Logo";
-import { Heart, ShoppingBagIcon, User } from "lucide-react";
 import { useState } from "react";
 import { MegaMenu } from "./MegaMenu";
 import { capitalizeFirstLetter } from "@/lib/utils";
-import { Search } from "./Search";
 import { Button } from "@/components/ui/button";
 import { api } from "@/trpc/react";
 import { UserMenu } from "./UserMenu";
@@ -28,18 +25,12 @@ export const navLinks: NavLink[] = [
   { name: "contacts", href: "/contacts" },
 ];
 
-export const sideNavLinks: [string, LucideIcon][] = [
-  // ["/wishlist", Heart],
-  ["/cart", ShoppingBagIcon],
-];
-
 export const Header = ({ collections }: { collections: Collections }) => {
   const [hoveredNavLink, setHoveredNavLink] = useState<NavLink | null>();
   const handleShowMenu = (navLink: NavLink) => setHoveredNavLink(navLink);
   const handleCloseMenu = () => setHoveredNavLink(null);
 
   const { data: session } = api.auth.me.useQuery();
-  const { data: userCart } = api.cart.list.useQuery(); // TODO: Skip this query when user session == null
 
   return (
     <header>
@@ -72,21 +63,6 @@ export const Header = ({ collections }: { collections: Collections }) => {
             {/* <Search onSearch={(value) => console.log(value)} /> */}
             {session?.user && (
               <div className="m-auto flex items-center gap-2">
-                {/* {sideNavLinks.map(([url, Icon]) => (
-                  <>
-                    <Link href="/cart">
-                      <div className="relative cursor-pointer">
-                        <Icon
-                          className="text-neutral-700 transition-colors hover:text-violet-700"
-                          size="30px"
-                        />
-                        <div className="absolute right-0 top-0 grid h-[18px] w-[18px] -translate-y-1 translate-x-1 place-items-center rounded-full bg-violet-700 text-[12px] text-white">
-                          {userCart?.cartItemCount}
-                        </div>
-                      </div>
-                    </Link>
-                  </>
-                ))} */}
                 <UserMenu user={session?.user} />
               </div>
             )}
