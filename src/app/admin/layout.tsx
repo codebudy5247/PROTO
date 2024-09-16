795119689c232fa7d235c37678367a6a48fdbd2c
codebudy5247 <a.shekhar5247@gmail.com>
2024-09-16 15:38:39 +0530

    update admin layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -4,11 +4,11 @@ import Header from "./_components/Header";
 const AdminLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <div className="grid h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
         <Sidenav />
-        <div className="flex flex-col">
+        <div className="flex h-screen flex-col">
           <Header />
-          {children}
+          <div className="flex-1 overflow-y-auto">{children}</div>
         </div>
       </div>
     </>
